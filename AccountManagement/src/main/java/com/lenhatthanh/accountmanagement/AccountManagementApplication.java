7ed0b8438153365bffe2087a9b2e8019e267931d
Lê Nhật Thanh <lenhatthanh20@gmail.com>
2023-08-23 01:42:54 +0700

    main: modify AccountManagementService

--- AccountManagement/src/main/java/com/lenhatthanh/accountmanagement/AccountManagementApplication.java
+++ AccountManagement/src/main/java/com/lenhatthanh/accountmanagement/AccountManagementApplication.java
@@ -2,8 +2,10 @@ package com.lenhatthanh.accountmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AccountManagementApplication {
 
 	public static void main(String[] args) {
