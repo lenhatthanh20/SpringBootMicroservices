d3aa93422ea2203df9b2319b4360758659fe9400
Lê Nhật Thanh <lenhatthanh20@gmail.com>
2023-08-24 01:00:24 +0700

    main: Test load balancing

--- UsersService/src/main/java/com/lenhatthanh/usersservice/controllers/UsersController.java
+++ UsersService/src/main/java/com/lenhatthanh/usersservice/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.lenhatthanh.usersservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+    private final Environment environment;
+
+    @Autowired
+    public UsersController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        return "Working on port " + environment.getProperty("local.server.port");
     }
 }
